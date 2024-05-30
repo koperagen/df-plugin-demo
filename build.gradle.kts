ac1e38605ee31a654dd87046e0554102b0018aa8
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2024-05-30 14:24:47 +0300

    Update dataframe to 0.13.1-dev-3153

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:dataframe:0.13.1-dev-3122")
-    kotlinCompilerPluginClasspath("org.jetbrains.kotlinx.dataframe:compiler-plugin-all:0.13.1-dev-3122")
+    implementation("org.jetbrains.kotlinx:dataframe:0.13.1-dev-3153")
+    kotlinCompilerPluginClasspath("org.jetbrains.kotlinx.dataframe:compiler-plugin-all:0.13.1-dev-3153")
     testImplementation(kotlin("test"))
 }
 
